3858167ca8c2d9b5683fd68c71bec8009b35c7d0
alex.shao <alex.shao@advantech.com.tw>
2018-03-13 14:39:59 +0800

    move openstack actions into main program

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,20 @@
 
 export HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [ "$(pwd)" != "{HERE}" ] && cd "${HERE}"
+export PATH=${HERE}/utils:$PATH
 unset HERE
 
-
 # log all HTTP requests/responses between Terraform and the OpenStack cloud
-export OS_DEBUG=1
-export TF_LOG="DEBUG"
+#export OS_DEBUG=1
+#export TF_LOG="DEBUG"
 
 # generate the private key to be widely used in OCF
-[ -e "bosh.pem" ] || ssh-keygen -t rsa -N '' -f bosh.pem
+if [ ! -e "bosh.pem" -a ! -e "bosh.pub" ]; then
+    ssh-keygen -t rsa -N '' -f bosh.pem
+    mv bosh.pem.pub bosh.pub
+fi
 
 # create all required resources on OpenStack
-openstack/create.sh
+terraform init openstack
+terraform apply -auto-approve openstack
 
